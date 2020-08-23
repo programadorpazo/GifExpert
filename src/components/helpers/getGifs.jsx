2e100ad9f5d61d485ba50b412c94791d612a6e91
programadorpazo <programador.pazo@gmail.com>
2020-08-23 08:09:47 -0500

    delte

--- src/components/helpers/getGifs.jsx
+++ src/components/helpers/getGifs.jsx
@@ -1,7 +1,8 @@
 
 const getGifs = async(category) => {
     //uso de category en la url encodeURI()
-    const url = 'http://localhost:55735/GifExpert';
+    //const url = 'http://localhost:55735/GifExpert';
+    const url = 'http://samirpazo-001-site1.dtempurl.com/GifExpert';
     const resp = await fetch( url );
     const data = await resp.json();
 
