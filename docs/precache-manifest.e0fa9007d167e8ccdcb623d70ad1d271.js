b510e367bb6d1f53a52da1fefec91fb0df65dd15
programadorpazo <programador.pazo@gmail.com>
2020-08-26 16:10:26 -0500

    docs agregado apikey

---
+++ docs/precache-manifest.e0fa9007d167e8ccdcb623d70ad1d271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2672a2549432e8222917e8a5581c264a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b8556761a5cb35ceca",
+    "url": "/static/css/main.716a2051.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b8556761a5cb35ceca",
+    "url": "/static/js/main.c7aaa3f3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
